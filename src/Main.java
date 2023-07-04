2d27c133a26e6f2469c05bde5dd64ae20003d8ec
Cesarribe <120605593+Cesarribe@users.noreply.github.com>
2023-07-04 13:24:52 -0300

    add as classes
    add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+
+        Jornalista jor = new Jornalista();
+        jor.add(new TV());
+        jor.notifyAll("Teste");
+
+
+    }
+}
